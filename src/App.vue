21824746edaa6a2bab32a8c10f1191f190baa83b
dj <845632255@qq.com>
2023-04-15 00:14:26 +0800

    修改配置项

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,17 @@
 
 <script>
   export default {
-    name: 'App'
+    name: 'App',
+    mounted() {
+      /**
+      * 解决 css 引入图片在 github pages 无法获取的问题
+      */
+      const { NODE_ENV } = process.env;
+      document.documentElement.className = NODE_ENV;
+    }
   }
 </script>
 
-<style>
+<style lang="less">
+@import "~@/assets/global.less";
 </style>
