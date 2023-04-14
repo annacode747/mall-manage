8820d0cf90c0824f7bc85ee94b02bd7c22ef7c6f
dj <845632255@qq.com>
2023-04-15 01:06:51 +0800

    github pages 配置修改

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,12 @@
       /**
       * 解决 css 引入图片在 github pages 无法获取的问题
       */
-      const { NODE_ENV } = process.env;
-      document.documentElement.className = NODE_ENV;
+//      const { NODE_ENV } = process.env;
+//      document.documentElement.className = NODE_ENV;
     }
   }
 </script>
 
 <style lang="less">
-@import "~@/assets/global.less";
+//@import "~@/assets/global.less";
 </style>
